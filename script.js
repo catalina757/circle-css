c7ba836d7893939b792804fce8700016bba92ea1
CatalinaCondrea <catalina.condrea@gmail.com>
2021-10-08 16:43:21 +0300

    css and script files

---
+++ script.js
@@ -0,0 +1,4 @@
+let pauseButton = document.getElementById("pauseButton");
+pauseButton.addEventListener("click", function () {
+    document.getElementById("circle2").classList.toggle("pause-animation");
+});
