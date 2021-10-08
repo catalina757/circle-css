ecb34d6b9d117517b104336c805aca5589c6dea0
CatalinaCondrea <catalina.condrea@gmail.com>
2021-10-08 17:01:30 +0300

    color variables in css

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let pauseButton = document.getElementById("pauseButton");
+
 pauseButton.addEventListener("click", function () {
     document.getElementById("circle2").classList.toggle("pause-animation");
 });
